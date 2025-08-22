5a88295c90419b5caa9f0ddf8f216c8d29f2145f
xReaLxSouLs <iliasgecko@gmail.com>
2025-08-23 00:57:46 +0100

    Update greet.py

--- 1_greeting_machine/greet.py
+++ 1_greeting_machine/greet.py
@@ -1,1 +1,9 @@
+def greet(name, time_of_day):
+    corrected_time = time_of_day.lower().capitalize()
+    corrected_name = name.lower().capitalize()
+    greeting = f"Good {corrected_time},{corrected_name}!"
+    return greeting
+
+
+print(greet("ILIASS", "morning"))
 
