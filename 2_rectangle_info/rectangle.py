78066db990eee0f287e98ad1a44921a59d20bc5c
xReaLxSouLs <iliasgecko@gmail.com>
2025-08-23 01:09:12 +0100

    rectangle.py

--- 2_rectangle_info/rectangle.py
+++ 2_rectangle_info/rectangle.py
@@ -1,1 +1,21 @@
+def rectangle_info(length, width):
+    area = length*width
+    perimeter = 2*(length+width)
+    return (area, perimeter)
 
+
+print(rectangle_info(5, 3))
+"""
+Calculates and returns the area and perimeter of a rectangle.
+
+Args:
+    length (float): The length of the rectangle
+    width (float): The width of the rectangle
+
+Returns:
+    tuple: A tuple containing (area, perimeter)
+
+Example:
+     rectangle_info(5, 3)
+    (15, 16)
+"""
